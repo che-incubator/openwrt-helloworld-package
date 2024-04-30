9c2a582112542f2f5c4febcedd64643e2ca47790
Oleksii Orel <oorel@redhat.com>
2024-04-30 04:54:02 +0300

    feat: add .vscode/launch.json file
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,8 @@ if [ ! -d "/projects/openwrt-helloworld/output" ]; then
   mkdir -p "/projects/openwrt-helloworld/output"
 fi
 
-cp cp -r /projects/openwrt/bin/targets/*/*/openwrt-*.gz /projects/openwrt-helloworld/output/
+if [ - f "/projects/openwrt-helloworld/output/image.raw" ]; then
+  rm -f "/projects/openwrt-helloworld/output/image.raw"
+fi
+
+cp -r /projects/openwrt/bin/targets/*/*/openwrt-*.gz /projects/openwrt-helloworld/output/
