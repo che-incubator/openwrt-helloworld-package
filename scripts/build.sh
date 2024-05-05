6c6677f70419ecc06ad21a4b7c557addbd64fe20
Oleksii Orel <oorel@redhat.com>
2024-05-06 02:25:49 +0300

    fix: config file
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,8 @@ set -e
 
 cd /projects/openwrt/ && make defconfig && make -j$(nproc)
 
-if [ ! -d "/projects/openwrt-helloworld/output" ]; then
-  mkdir -p "/projects/openwrt-helloworld/output"
+if [ ! -d /projects/openwrt-helloworld/output ]; then
+  mkdir -p /projects/openwrt-helloworld/output
 fi
 
-if [ - f "/projects/openwrt-helloworld/output/image.raw" ]; then
-  rm -f "/projects/openwrt-helloworld/output/image.raw"
-fi
-
-cp -r /projects/openwrt/bin/targets/*/*/openwrt-*.gz /projects/openwrt-helloworld/output/
+cp -r /projects/openwrt/bin/targets/*/*/openwrt-*combined.img.gz /projects/openwrt-helloworld/output/
